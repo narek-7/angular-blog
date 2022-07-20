eca8eab20f55c1cf4279060add44e9168aae34f3
Narek <narek1985ch@gmail.com>
2022-07-20 23:04:27 +0200

    implements the structures of the admin-layout, post-page and navigation of admin pages.

--- src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.css']
+  styleUrls: ['./admin-layout.component.css'],
 })
 export class AdminLayoutComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logOut(e: Event): void {
+    e.preventDefault();
+    this.router.navigate(['/admin', 'login']);
   }
-
 }
