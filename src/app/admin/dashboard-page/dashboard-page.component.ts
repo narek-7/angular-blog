b192da3710520de09cbc5a2ee70109520425164c
Narek <narek1985ch@gmail.com>
2022-08-13 22:11:27 +0200

    Deleting a post

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -11,21 +11,28 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   posts: any;
   searchString: string = '';
   postSub: Subscription = new Subscription();
+  deleteSub: Subscription = new Subscription();
 
   constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {
     this.postSub = this.postsService.getAll().subscribe((posts: any) => {
       this.posts = posts;
-      console.log(posts);
     });
   }
 
-  remove(id: string) {}
+  remove(id: any) {
+    this.deleteSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter((post: any) => post.id !== id);
+    });
+  }
 
   ngOnDestroy() {
     if (this.postSub) {
       this.postSub.unsubscribe();
+    }
+    if (this.deleteSub) {
+      this.deleteSub.unsubscribe();
     }
   }
 }
