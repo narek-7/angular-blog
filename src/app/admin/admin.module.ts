803198c9f37637ef2518d0b31d64a694f87ebc59
Narek <narek1985ch@gmail.com>
2022-08-13 23:22:47 +0200

    Post edit page implemented

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
         canActivate: [AuthGuard],
       },
       {
-        path: 'edit/:id/edit',
+        path: 'post/:id/edit',
         component: EditPageComponent,
         canActivate: [AuthGuard],
       },
--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { PostsService } from 'src/app/shared/posts.service';
+import { switchMap } from 'rxjs/operators';
+import { Post } from 'src/app/shared/interfaces';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
-  styleUrls: ['./edit-page.component.css']
+  styleUrls: ['./edit-page.component.css'],
 })
 export class EditPageComponent implements OnInit {
+  post: any;
+  form: any;
 
-  constructor() { }
+  constructor(
+    private postsService: PostsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.params
+      .pipe(
+        switchMap((params: Params) => {
+          return this.postsService.getById(params['id']);
+        })
+      )
+      .subscribe((post: Post) => {
+        this.form = new FormGroup({
+          title: new FormControl(post.title, Validators.required),
+          text: new FormControl(post.text, Validators.required),
+        });
+      });
   }
 
+  submit() {}
 }
